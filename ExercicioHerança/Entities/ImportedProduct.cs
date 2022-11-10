649e855b902f75c02d76921033981dc8b23ceb91
Jose <ajose9975@gmail.com>
2022-11-10 16:30:00 -0300

    Add project files.

---
+++ ExercicioHerança/Entities/ImportedProduct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioHerança.Entities {
+    internal class ImportedProduct : Product {
+        public double CustomsFee { get; set; }
+        public ImportedProduct(string name, double price, double customsFee) : base(name, price) {
+            CustomsFee = customsFee;
+        }
+        public override string priceTag() {
+            return Name + " $ " + Price.ToString("F2", CultureInfo.InvariantCulture) + $" (Customs fee: $ {CustomsFee.ToString("F2", CultureInfo.InvariantCulture)})";
+        }
+    }
+}
